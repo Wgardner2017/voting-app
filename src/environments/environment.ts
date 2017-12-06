40a8762cd90094cfd84517dd5fee4bacf83dc7d2
Wgardner2017 <30301979+Wgardner2017@users.noreply.github.com>
2017-12-06 17:56:40 -0500

    yarn update scss changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA0K0DsSJuVZs1wqLLOkJm_RyH2iDT8QHk',
+    authDomain: 'voting-app-warrengardner.firebaseapp.com',
+    databaseURL: 'https://voting-app-warrengardner.firebaseio.com',
+    projectId: 'voting-app-warrengardner',
+    storageBucket: 'voting-app-warrengardner.appspot.com',
+    messagingSenderId: '963198494990'
+  }
 };
